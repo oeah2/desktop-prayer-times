0f472c180d99fd799490883c5e7ebd78d366f810
oeah2 <49606652+oeah2@users.noreply.github.com>
2021-05-30 16:57:32 +0200

    Created own module for file operations

---
+++ src/file.c
@@ -0,0 +1,29 @@
+#include "file.h"
+#include <assert.h>
+
+size_t file_find_length(FILE* file)
+{
+    size_t ret = 0;
+    if(file) {
+        rewind(file);
+        int fseek_result = fseek(file, 0, SEEK_END);
+        assert(!fseek_result);
+        ret = ftell(file);
+        rewind(file);
+    }
+    return ret;
+}
+
+bool file_read_all(FILE* file, size_t max_len, char dest[max_len])
+{
+    bool ret = false;
+    if(file) {
+        size_t file_length = file_find_length(file);
+        if(file_length > max_len) return ret;
+
+        size_t read_bytes = fread(dest, sizeof(char), file_length, file);
+        if(read_bytes < (file_length - 1)) return false;    // depends on library implementation(?)
+        ret = true;
+    }
+    return ret;
+}
---
+++ src/file.h
@@ -0,0 +1,25 @@
+//#ifndef FILE_H_INCLUDED
+//#define FILE_H_INCLUDED
+#include <stddef.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+/** \brief Returns length of file
+ *
+ * \param file FILE* of which the lengh shall be determined
+ * \return size_t file length
+ *
+ */
+size_t file_find_length(FILE* file);
+
+/** \brief Reads all content of file into destination array
+ *
+ * \param file FILE*
+ * \param max_len size_t max size to be read, including terminating 0
+ * \param dest[max_len] char
+ * \return bool true on success, false otherwise
+ *
+ */
+bool file_read_all(FILE* file, size_t max_len, char dest[max_len]);
+
+//#endif // FILE_H_INCLUDED
